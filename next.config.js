73a45badb5868fac9d0d11e82861d0e467e2f23d
Phil Leggetter <phil@leggetter.co.uk>
2022-11-09 15:00:36 +0000

    chore: home page update for template

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ['static.ably.dev'],
+  },
 }
 
 module.exports = nextConfig
