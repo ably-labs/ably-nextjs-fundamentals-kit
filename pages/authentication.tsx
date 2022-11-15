28fe306bf5727c24b7db6c0ccb463a44440ca8e3
Phil Leggetter <phil@leggetter.co.uk>
2022-11-15 18:07:50 +0000

    chore: remove/update auth comments

--- pages/authentication.tsx
+++ pages/authentication.tsx
@@ -16,7 +16,6 @@ export default function Authentication() {
   useEffect(() => {
     const handleConnectionStateChange = (stateChange: Ably.Types.ConnectionStateChange) => {
       setLogs(prev => [...prev, new LogEntry(`Connection state change: ${stateChange.previous} -> ${stateChange.current}`)])
-      // setLogs([...logs, new LogEntry(`Connection state change: ${stateChange.previous} -> ${stateChange.current}`)])
 
       setConnectionState(stateChange.current)
     }
@@ -25,7 +24,7 @@ export default function Authentication() {
     ably.connection.on(handleConnectionStateChange)
 
     return () => {ably.connection.off()}
-  }, []) // Only run on the client
+  }, []) // Only run the client
 
   const connectionToggle: MouseEventHandler =  (_event: MouseEvent<HTMLButtonElement>) => {
     const ably = assertConfiguration()
