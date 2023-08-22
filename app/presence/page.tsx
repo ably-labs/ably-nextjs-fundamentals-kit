e44e62a99c4351e5737ab8c658d0783d67a77b56
ablydevin <136481955+ablydevin@users.noreply.github.com>
2023-08-21 21:24:11 -0400

    Peesence example work

--- app/presence/page.tsx
+++ app/presence/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import * as Ably from 'ably';
-import { AblyProvider, useChannel, usePresence } from "@ably-labs/react-hooks"
+import { AblyProvider, useAbly, useChannel, usePresence } from "@ably-labs/react-hooks"
 import Logger, { LogEntry } from '../../components/logger';
-import { MouseEvent, useState, FormEvent, ChangeEvent, useCallback } from 'react'
+import { MouseEvent, useState, FormEvent, ChangeEvent, useCallback, useEffect } from 'react'
 import NavBar from '../../components/navbar';
 
 
@@ -14,7 +14,8 @@ const presenceActionIcon = new Map<string, string>([
 
 export default function Presence() {
     const options = {
-        authUrl:'/api/authentication/token-auth'
+        authUrl:'/api/authentication/token-auth',
+        autoConnect: false,
        }
     
     return (
@@ -30,20 +31,31 @@ function PresenceMessages() {
    const [isUsernameValid, setIsUsernameValid] = useState<boolean>(false)
    const [onlineUsers, setOnlineUsers] = useState<string[]>([])
 
-   const handlePresenceMessage = useCallback((message: Ably.Types.PresenceMessage) => {
-     console.log('handlePresenceMessage', message.action, message.clientId, new Date());
+   const client = useAbly();
 
-     if(message.action === 'enter' || message.action === 'present') {
-       setOnlineUsers(prev => {
-         if(prev.includes(message.clientId) === false) {
-           return [...prev, message.clientId]
-         }
-         else {
-           return prev
-         }
-       })
-    }
-    else { // user has left
+  useEffect(() =>{
+    if (isUsernameValid === false) return;
+    client.auth.authorize(
+      { clientId: username }, 
+      { authUrl:'/api/authentication/token-auth', authMethod: 'POST'}
+    );
+  });
+
+    //this should only happen once we have a valid user name
+  const { presenceData, updateStatus } = usePresence("room", {'status':'available'}, (message) => {
+    console.log(message);
+
+    if(message.action === 'enter' || message.action === 'present') {
+      setOnlineUsers(prev => {
+        if(prev.includes(message.clientId) === false) {
+          return [...prev, message.clientId]
+        }
+        else {
+          return prev
+        }
+      })
+    } 
+    else {
       setOnlineUsers(prev => prev.filter(username => {
         const keep: boolean = username !== message.clientId
         return keep
@@ -51,11 +63,15 @@ function PresenceMessages() {
     }
 
     setLogs(prev => [...prev, new LogEntry(`action: ${message.action} clientId: ${message.clientId}`)])
-  }, [])
-
-  const { updateStatus } = usePresence("room", (presenceUpdate:Ably.Types.PresenceMessage) => {
-    console.log(presenceUpdate);
   });
+  
+
+  // const presentClients = presenceData.map((msg, index) => (
+  //   <li key={index}>
+  //       {msg.clientId}: {JSON.stringify(msg.data)}
+  //   </li>
+  // ));
+
 
 //   useEffect(() => {
 //     // The first requirement is to have a valid username
@@ -114,43 +130,52 @@ function PresenceMessages() {
    }
 
   return (
-      <div className="container mx-auto">
+
+    <div className="container mx-auto">
+      <header>
         <NavBar />
-        <p>
-          Presence with Ably allows you to keep track of devices that are present on a channel. This is great for tracking if a device is online or offline or indicating if a user is in a chat room when using Ably for Chat.
-        </p>
-        <p>
-          <a href="/presence" target="_blank">Open this page</a> in another tab to see more users enter and leave the presence channel.
-        </p>
-        { isUsernameValid === false?
-        <section>
-          <form onSubmit={handleFormSubmit}>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text" 
-              key="username"
-              value={username} 
-              placeholder="What is your username?"
-              onChange={handleUsernameInputChange}
-            />
-            <button>Join</button>
-          </form>
-        </section>
-        :
-        <section>
-          <ul>
-            {onlineUsers.map((username: string) => {
-              return <li key={username}>
-                {presenceActionIcon.get('enter')} {username}
-              </li>
-            })}
-          </ul>
-          <button onClick={handleLeaveClick}>Leave</button>
-        </section>
-        }
-        <section>
-          <Logger logEntries={logs} />
-        </section>
+      </header>
+      <section className="bg-white">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl">
+          <p className="mb-8 text-sm font-normal text-gray-500 text-center">
+            Presence with Ably allows you to keep track of devices that are present on a channel. This is great for tracking if a device is online or offline or indicating if a user is in a chat room when using Ably for Chat.  <a href="/presence" target="_blank">Open this page</a> in another tab to see more users enter and leave the presence channel.
+          </p>
+
+          { isUsernameValid === false?
+          <section>
+            <form onSubmit={handleFormSubmit}>
+              <label htmlFor="username">Username</label>
+              <input
+                className="block w-96 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" id="message" 
+                type="text" 
+                key="username"
+                value={username} 
+                placeholder="What is your username?"
+                onChange={handleUsernameInputChange}
+              />
+              <button className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Join</button>
+            </form>
+          </section>
+          :
+          <section>
+            <ul>
+              {onlineUsers.map((username: string) => {
+                return <li key={username}>
+                  {presenceActionIcon.get('enter')} {username}
+                </li>
+              })}
+            </ul>
+            <button className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300" onClick={handleLeaveClick}>Leave</button>
+          </section>
+          }
+          <section>
+            <Logger logEntries={logs} />
+          </section>
+
         </div>
+      </section>
+    </div>
+
+
   )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,7 +20,7 @@ export default function NavBar() {
         <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
             </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
