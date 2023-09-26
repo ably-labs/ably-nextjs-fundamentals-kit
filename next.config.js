3d5176894c3d904c5cca8b4b8ae956118961c121
ablydevin <136481955+ablydevin@users.noreply.github.com>
2023-09-26 12:55:22 -0400

    Adding config to handle nextjs warnings

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     domains: ['static.ably.dev'],
   },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },  
 }
 
 module.exports = nextConfig
